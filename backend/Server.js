fb94579f9ed343a2a493d28c486e004953237ab5
H. S. Kim <saviorkhs@gmail.com>
2025-04-07 14:27:14 +0900

    [FEAT] logout & signup api
    1. 회원가입, 로그아웃 기능 추가
    2. 인증 함수 분리
    3. 추가 스키마 주석화

--- backend/Server.js
+++ backend/Server.js
@@ -9,6 +9,8 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const User = require('./db/User');
+const authToken = require('./lib/AuthToken');
+
 
 const app = express();
 app.use(express.json());
@@ -39,16 +41,20 @@ mongoose.connect(MONGO_URI)
 
 
 /*
+  API
+*/
+
+/*
     [POST] /api/v1/user/login
     1. Input 
       - Body : { id: String, pw: String }
     
     2. Output
      a. Success
-        - status 200 { msg : '회원가입 성공' }
+        - status 200 { success : true, msg : '로그인 성공' }
         - cookie { authToken }
      b. Fail
-        - status 400|500 { msg : '회원가입 실패' }
+        - status 400|500 { success : false, msg : '로그인 실패' }
 */
 app.post('/api/v1/user/login', async (req, res) => {
 
@@ -61,7 +67,7 @@ app.post('/api/v1/user/login', async (req, res) => {
     const user = await User.findOne({ id });
 
     if (!user || user.pw !== pw) {
-      return res.status(400).json({ msg: '아이디/비밀번호를 다시 확인해주세요.' });
+      return res.status(400).json({ success : false, msg: '아이디/비밀번호를 다시 확인해주세요.' });
     }
 
     const token = jwt.sign({ id: user._id }, JWT_SECRET, {
@@ -73,44 +79,104 @@ app.post('/api/v1/user/login', async (req, res) => {
       secure: false,
       sameSite: 'Lax',
       maxAge: 3600000
-    }).status(200).json({ msg: '로그인 성공' });
+    }).status(200).json({ success : true, msg: '로그인 성공' });
 
   } catch (err) {
-    res.status(500).json({ msg: '잠시 후에 로그인해주세요.' });
+    res.status(500).json({ success : false, msg: '서버 오류' });
   }
 
 });
 
 /*
     [GET] /api/v1/user/auth
+
+    0. Import
+      - Fucntion : authToken
+    
     1. Input 
       - Cookie : { authToken }
     
     2. Output
      a. Success
-        - status 200 { msg : '유효한 토큰' , user : flag }
+        - status 200 { success : true, msg : '유효한 토큰' , user : flag }
      b. Fail
-        - status 401 { msg : '만료된 토큰' }
+        - status 401 { success : false, msg : '만료된 토큰' }
 
 */
-app.get('/api/v1/user/auth', (req, res) => {
-
-  const token = req.cookies.authToken;
+app.get('/api/v1/user/auth', authToken, (req, res) => {
 
   // to-do
-  try {
-    const flag = jwt.verify(token, JWT_SECRET); 
-
     res.status(200).json({
+      success : true,
       msg: '유효한 토큰',
-      user: flag 
+      user: req.user 
     });
 
-  } catch (err) {
-    res.status(401).json({ msg: '만료된 토큰' });
-  }
 });
 
+/*
+    [GET] /api/v1/user/logout
+
+    0. Import
+      - Fucntion : authToken
+    
+    1. Input 
+      - Cookie : { authToken }
+    
+    2. Output
+     a. Success
+        - status 200 { success : true, msg : '로그아웃 완료'}
+     b. Fail
+        - status 401 { success : false, msg : '만료된 토큰' }
+
+*/
+app.get('/api/v1/user/logout', authToken, (req, res) => {
+
+  res.clearCookie('authToken');
+  res.status(200).json({ success : true, msg: '로그아웃 완료' });
+
+});
+
+/*
+    [POST] /api/v1/user/signup
+
+    1. Input 
+      - Body : { id: String, pw: String }
+    
+    2. Output
+     a. Success
+        - status 200 { success : true,  msg : '회원가입 성공'}
+     b. Fail
+        - status 400 { success : false, msg : '아이디 존재' }
+        - status 500 { success : false, msg : '서버 오류' }
+
+*/
+app.post('/api/v1/user/signup', async (req, res) => {
+  
+  const id = req.body.id;
+  const pw = req.body.pw;
+
+  try {
+
+    const flag = await User.findOne({ id });
+
+    if (flag) {
+      return res.status(400).json({ success : false, sg: '아이디 존재' });
+    }
+
+    const user = new User({ id, pw });
+    await user.save();
+
+    res.status(201).json({ success : true, msg: '회원가입 성공' });
+
+  } catch (err) {
+
+    console.error(err);
+    res.status(500).json({ success : false, msg: '서버 오류' });
+
+  }
+
+});
 
 app.listen(port, () => {
     console.log(`HOST : http://localhost:${port}`)
---
+++ backend/lib/AuthToken.js
@@ -0,0 +1,33 @@
+/*
+  Config
+*/
+
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'secret_key';
+
+/*
+    Function AuthToken
+
+    - 쿠키<->JWT 간 일치/불일치 확인
+
+*/
+
+function AuthToken(req, res, next) {
+  
+const token = req.cookies.authToken;
+
+  try {
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded; 
+    next();
+
+  } 
+  catch (err) {
+    
+    return res.status(401).json({ success : false, msg: '만료된 토큰' });
+
+  }
+}
+
+module.exports = AuthToken;
